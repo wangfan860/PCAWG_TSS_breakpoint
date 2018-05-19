c948c7bdff28fead994d1241f8e6e72714225f1e
fan wang <wangfan860@gmail.com>
2018-05-19 16:59:40 -0500

    chore(temp): change log path

--- qsub_template.sh
+++ qsub_template.sh
@@ -3,7 +3,7 @@
 #PBS -l nodes=1:ppn=1
 #PBS -l mem=4gb
 #PBS -l walltime=05:59:59
-#PBS -o /group/lyang-lab/fan/pcawg_data/fwang1_XX_CHUNK_XX.stdout.log
-#PBS -e /group/lyang-lab/fan/pcawg_data/fwang1_XX_CHUNK_XX.stderr.log
+#PBS -o /group/lyang-lab/fan/pcawg_data/log/fwang1_XX_CHUNK_XX.stdout.log
+#PBS -e /group/lyang-lab/fan/pcawg_data/log/fwang1_XX_CHUNK_XX.stderr.log
 
 /apps/software/gcc-6.2.0/python/3.6.0/bin/python3 /group/lyang-lab/fan/pcawg_data/PCAWG_TSS_breakpoint/distance_table.py -g XX_CHUNK_XX
