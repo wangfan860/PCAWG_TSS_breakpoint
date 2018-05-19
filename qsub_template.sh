621d6653ed941cff30a07dd53b31dee1ae636bd7
fan wang <wangfan860@gmail.com>
2018-05-19 17:11:22 -0500

    fix(module): import module

--- qsub_template.sh
+++ qsub_template.sh
@@ -6,4 +6,7 @@
 #PBS -o /group/lyang-lab/fan/pcawg_data/log/fwang1_XX_CHUNK_XX.stdout.log
 #PBS -e /group/lyang-lab/fan/pcawg_data/log/fwang1_XX_CHUNK_XX.stderr.log
 
+module load gcc/6.2.0
+module load python/3.6.0
+
 /apps/software/gcc-6.2.0/python/3.6.0/bin/python3 /group/lyang-lab/fan/pcawg_data/PCAWG_TSS_breakpoint/distance_table.py -g XX_CHUNK_XX
