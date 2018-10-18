867337ba90e99d573a6d747dc85c4fa25145e416
fan wang <wangfan860@gmail.com>
2018-10-18 10:24:15 -0500

    add merge chunk

--- merge_cnv_segment.py
+++ merge_cnv_segment.py
@@ -1,38 +1,25 @@
 import pandas as pd
 import os
 import glob
-import argparse
-def get_args():
-    '''
-    Loads the parser
-    '''
-    # Main parser
-    parser = argparse.ArgumentParser(description="merge tcga fpkm")
-    # Args
-    required = parser.add_argument_group("Required input parameters")
-    # Metadata from input table
-    required.add_argument('-g', '--gene_block', required=True, help='file block.')
-    return parser.parse_args()
+
 
 def chunks(l, n):
     for i in range(0, len(l), n):
         yield l[i:i + n]
 
-if __name__ == '__main__':
-    args = get_args()
-    gb = int(args.gene_block)
-    input=glob.glob('/gpfs/data/lyang-lab/users/fan/breakpoint_tcga/CNV_bedtool/CNV/*merge.csv')
-    total_input_list=list(chunks(input, 10))
-    input_list = total_input_list[gb]
+input=glob.glob('/gpfs/data/lyang-lab/users/fan/breakpoint_tcga/CNV_bedtool/CNV/*merge.csv')
+total_input_list=list(chunks(input, 10))
+
+for i in list(range(0,len(total_input_list))):
+    input_list = total_input_list[i]
+    print(i)
     ref=pd.read_csv('/gpfs/data/lyang-lab/users/fan/breakpoint_tcga/gencode_hg38_56354gene.csv')
     ref1=ref[['gene_id2']]
     ref1.columns=['gene']
-
     for df_ in input_list:
         print(os.path.basename(df_))
         df_csv=pd.read_csv(df_,index_col=0)
         df_csv.columns=['gene',os.path.basename(df_)]
         df_csv_nodup=df_csv.drop_duplicates(subset='gene', keep='first')
         ref1=pd.merge(ref1,df_csv_nodup, on='gene', how='outer')
-
-    ref1.to_csv('/gpfs/data/lyang-lab/users/fan/breakpoint_tcga/merged_cnv_segment.{}.csv'.format(gb), index=False)
+    ref1.to_csv('/gpfs/data/lyang-lab/users/fan/breakpoint_tcga/merged_cnv_segment.{}.csv'.format(i), index=False)
--- merge_expr_tcga.py
+++ merge_expr_tcga.py
@@ -1,37 +1,25 @@
 import pandas as pd
 import os
 import glob
-import argparse
-def get_args():
-    '''
-    Loads the parser
-    '''
-    # Main parser
-    parser = argparse.ArgumentParser(description="merge tcga fpkm")
-    # Args
-    required = parser.add_argument_group("Required input parameters")
-    # Metadata from input table
-    required.add_argument('-g', '--gene_block', required=True, help='file block.')
-    return parser.parse_args()
+
 
 def chunks(l, n):
     for i in range(0, len(l), n):
         yield l[i:i + n]
 
-if __name__ == '__main__':
-    args = get_args()
-    gb = int(args.gene_block)
+input = glob.glob('/gpfs/data/lyang-lab/users/fan/breakpoint_tcga/RNA/*.txt')
+total_input_list=list(chunks(input, 10))
 
-    input = glob.glob('/gpfs/data/lyang-lab/users/fan/breakpoint_tcga/RNA/*.txt')
-    total_input_list=list(chunks(input, 10))
-    input_list = total_input_list[gb]
+
+
+for i in range(0,len(total_input_list)):
+    input_list = total_input_list[i]
+    print(i)
     test1=pd.read_csv('/gpfs/data/lyang-lab/users/fan/breakpoint_tcga/RNA/7ea980bb-9a4f-4adf-bebd-9c7c56cc5651.FPKM-UQ.txt', sep='\t', header=None)
     test1.columns=['gene','fpkm']
     test=test1[['gene']]
-
-
     for df_ in input_list:
         df_csv=pd.read_csv(df_, sep='\t', header=None)
         df_csv.columns=['gene',os.path.basename(df_)]
         test=pd.concat([test,df_csv[[os.path.basename(df_)]]], axis=1)
-    test.to_csv('/gpfs/data/lyang-lab/users/fan/breakpoint_tcga/merged_fpkm.{}.csv'.format(gb), index=False)
+    test.to_csv('/gpfs/data/lyang-lab/users/fan/breakpoint_tcga/merged_fpkm.{}.csv'.format(i), index=False)
